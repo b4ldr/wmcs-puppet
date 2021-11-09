704e95d06f025704cc2de32030d81bd94d79ccf0
jbond <github@johnbond.org>
2021-11-09 19:53:42 +0100

    switch to wmcs exim
    this parameteres shouldn;t be calling globals

--- site-modules/profile/manifests/mail/default_mail_relay.pp
+++ site-modules/profile/manifests/mail/default_mail_relay.pp
@@ -1,6 +1,7 @@
 class profile::mail::default_mail_relay (
     Boolean $enabled  = lookup('profile::mail::default_mail_relay::enabled'),
     String  $template = lookup('profile::mail::default_mail_relay::template'),
+    Array[String] $mail_smarthost = lookup('mail_smarthost')
 ) {
     if $enabled {
         class { 'exim4':
